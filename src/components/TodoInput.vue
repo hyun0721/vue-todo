bde7dd8ec3161a41b2e74a9b54631bf047d30a4d
hyun0721 <hyunee721@gmail.com>
2022-09-15 22:43:25 +0900

    Component Setting

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Vue 템플릿은 기본적으로 div태그를 하위에 '1개' 두고 그 하위에 디자인한다. -->
+    <!-- 컴포넌트 규칙..! -->
+    <div>
+        input
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
