23d9ba2459053ddfe6e633338356184349df98d0
hyun0721 <hyunee721@gmail.com>
2022-09-27 22:58:07 +0900

    컴포넌트 기초 스타일링 - scoped 개념

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it !</h1>
+    </header>
 </template>
 
 <script>
@@ -10,6 +10,11 @@ export default {
 }
 </script>
 
-<style>
-
+<!-- scoped: 스타일이 상속되지 않고 해당 컴포넌트에서만 유지됨. -->
+<style scoped>
+h1 {
+    color: #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
 </style>
