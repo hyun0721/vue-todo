5f1417731f60befcb3502d3e213e2b9d065108c1
hyun0721 <hyunee721@gmail.com>
2022-09-28 23:05:26 +0900

    1. 입력 컴포넌트, 리스트 구현 2. 입출력 양방향 바인딩

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,17 +1,63 @@
 <template>
     <!-- Vue 템플릿은 기본적으로 div태그를 하위에 '1개' 두고 그 하위에 디자인한다. -->
     <!-- 컴포넌트 규칙..! -->
-    <div>
-        input
+    <div class="inputBox shadow">
+        <!-- v-model : 양방향 바인딩, 뷰 인스턴스에 반영됨. -->
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fa-solid fa-plus"></i>
+        </span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function(){
+            localStorage.setItem('INPUT_BOX', this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput: function(){
+            this.newTodoItem = null;
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+
+input:focus{
+    outline: none;
+}
+
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+
+.inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+}
+
+.addContainer{
+    float:right;
+    background: linear-gradient(to right, #5478fb, #8763fb);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn{
+    color:white;
+    vertical-align: middle;
+}
 
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        list
+        <ul>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+                {{ todoItem }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+            todoItems: []
+        }
+    },
+    created: function(){
+        console.log('created');
+        if(localStorage.length > 0){
+            for(let locStKey of Object.keys(localStorage)){
+                if(locStKey != "loglevel:webpack-dev-server"){
+                    this.todoItems.push(localStorage[locStKey]);
+                }
+            }
+        }
+    }
 }
 </script>
 
